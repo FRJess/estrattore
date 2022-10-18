3b14dfcc3fa97712274a3ff9a39fcd3c86d21b6e
FR_Jess <fr_jess@icloud.com>
2022-10-18 16:11:48 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -37,15 +37,37 @@ const classe75 = [
   'Andrea	Zollini'
 ];
 
-// CHECK PRIMA DI CHIUDERE
-
 const button = document.querySelector("#btn-input-on");
+const studentiEstratti = [];
+const output = document.querySelector('#output');
 
 //random
 button.addEventListener('click', function(){
-  const randomIndex = Math.floor(Math.random() * classe75.length  );
-  document.querySelector('#output').innerHTML = classe75[randomIndex];
+  // 1 estrarre un numero compreso fra 0 e l'ultimo elemento dell'array
+  // 2 se il mome estratto (corrispondente all'indice del numero estratto) è già presente nell'array di studenti estratti devo estrarre un altro numero funo a quando ne trovo uno non estratto
+  // 3 quando trovo un nuovo nome lo aggiungo all'elenco di studenti estratti
+  // 4 se l'elenco di studenti estratti è lungo uguale all'elenco della classe finisce
+  let isNomeEstratto = false;
 
-});
+  if(studentiEstratti.length < classe75.length){
 
+    while(!isNomeEstratto){
+      const randomIndex = Math.floor(Math.random() * classe75.length );
+      const nomeEstratto =  classe75[randomIndex];
+      console.log(nomeEstratto,studentiEstratti);
+      if(!studentiEstratti.includes(nomeEstratto)){
+        studentiEstratti.push(nomeEstratto);
+        isNomeEstratto = true;
+      }
+      output.innerHTML = nomeEstratto;
+    }
+
+  }else{
+    // FINE
+    output.innerHTML = 'TUTTI GLI STUDENTI SONO STATI ESTRATTI';
+    console.log('fine');
+  }
+
+
+}); 
 
